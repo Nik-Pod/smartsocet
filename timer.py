a859ce42f97be08232e6e7a9766908077e8b26d8
Nik-Pod <nickolay.podkolzin@gmail.com>
2019-11-15 11:26:37 +0300

    timer is ready

--- timer.py
+++ timer.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-rel1 = 
-rel2 =
-rel3 =
-rel4 = 
-led1 =
-led2 = 
-led3 =
-led4 =
+rel1 = 21
+rel2 = 20
+rel3 = 16
+rel4 = 12
+led1 = 26
+led2 = 19
+led3 = 13
+led4 = 6
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(rel1, GPIO.OUT)
@@ -22,7 +22,85 @@ GPIO.setup(led4, GPIO.OUT)
 
 t = open('time.txt')
 m = open('many.txt')
+p = open('port.txt')
 many = m.read()
 timing = t.read()
+port = p.read()
 if timing == 'c':
-    GPIO.
+    if port == 1:
+        GPIO.output(rel1, True)
+        GPIO.output(led1, True)
+        sleep(int(many))
+        GPIO.output(rel1, False)
+        GPIO.output(led1, False)
+    elif port == 2:
+        GPIO.output(rel2, True)
+        GPIO.output(led2, True)
+        sleep(int(many))
+        GPIO.output(rel2, False)
+        GPIO.output(led2, False)
+    elif port == 3:
+        GPIO.output(rel3, True)
+        GPIO.output(led3, True)
+        sleep(int(many))
+        GPIO.output(rel3, False)
+        GPIO.output(led3, False)
+    elif port == 4:
+        GPIO.output(rel4, True)
+        GPIO.output(led4, True)
+        sleep(int(many))
+        GPIO.output(rel4, False)
+        GPIO.output(led4, False)
+elif timing == 'м':
+    if port == 1:
+        GPIO.output(rel1, True)
+        GPIO.output(led1, True)
+        sleep(int(many) * 60)
+        GPIO.output(rel1, False)
+        GPIO.output(led1, False)
+    elif port == 2:
+        GPIO.output(rel2, True)
+        GPIO.output(led2, True)
+        sleep(int(many) * 60)
+        GPIO.output(rel2, False)
+        GPIO.output(led2, False)
+    elif port == 3:
+        GPIO.output(rel3, True)
+        GPIO.output(led3, True)
+        sleep(int(many) * 60)
+        GPIO.output(rel3, False)
+        GPIO.output(led3, False)
+    elif port == 4:
+        GPIO.output(rel4, True)
+        GPIO.output(led4, True)
+        sleep(int(many) * 60)
+        GPIO.output(rel4, False)
+        GPIO.output(led4, False)
+elif timing == 'ч':
+    if port == 1:
+        GPIO.output(rel1, True)
+        GPIO.output(led1, True)
+        sleep(int(many))
+        GPIO.output(rel1, False)
+        GPIO.output(led1, False)
+    elif port == 2:
+        GPIO.output(rel2, True)
+        GPIO.output(led2, True)
+        sleep(int(many))
+        GPIO.output(rel2, False)
+        GPIO.output(led2, False)
+    elif port == 3:
+        GPIO.output(rel3, True)
+        GPIO.output(led3, True)
+        sleep(int(many))
+        GPIO.output(rel3, False)
+        GPIO.output(led3, False)
+    elif port == 4:
+        GPIO.output(rel4, True)
+        GPIO.output(led4, True)
+        sleep(int(many))
+        GPIO.output(rel4, False)
+        GPIO.output(led4, False)
+t.close()
+m.close()
+p.close()
